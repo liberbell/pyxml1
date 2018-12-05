f32a267e2ad077ade5481575c4bd6b08c2dd9126
liberbell <liberbell@gmail.com>
2018-12-06 08:52:18 +0900

    urllib3

--- urllib_start.py
+++ urllib_start.py
@@ -3,6 +3,10 @@ import urllib.request
 def main():
     url = 'http://httpbin.org/xml'
 
+    result = urllib.request.urlopen(url)
+
+    print('Result code: {0}'.format(result.status))
+
     print('Headers:----------------------')
 
 
