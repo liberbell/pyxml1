b4b9ec5f6405a7ef976650599612b0a38de79db8
liberbell <liberbell@gmail.com>
2018-12-07 08:42:00 +0900

    urllib_err3

--- urllib_err.py
+++ urllib_err.py
@@ -3,12 +3,12 @@ from http import HTTPStatus
 
 def main():
     # url='http://'non-such-server.org'
-    # url='http://httpbin.org/status/404'
-    url='http://httpbin.org/html'
+    url='http://httpbin.org/status/404'
+    # url='http://httpbin.org/html'
 
     result = urllib.request.urlopen(url)
     print('Result code: {}'.format(result.status))
-    if result.getcode() == HTTPStatus.OK:
+    if (result.getcode() == HTTPStatus.OK):
         print(result.read().decode('UTF-8'))
 
 if __name__ == '__main__':
