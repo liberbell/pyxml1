9cd6120ebf1c06caf1ca740be0402b5c4d9d3d30
liberbell <liberbell@gmail.com>
2018-12-07 08:38:02 +0900

    urllib_err1

--- urllib_err.py
+++ urllib_err.py
@@ -1,6 +1,14 @@
 import urllib.request
 
 def main():
-    url='http://'
-    url='http://httpbin.org/status/404'
+    # url='http://'non-such-server.org'
+    # url='http://httpbin.org/status/404'
     url='http://httpbin.org/html'
+
+    result = urllib.request.urlopen(url)
+    print('Result code: {}'.format(result.status))
+    if result.status == 200:
+        print(result.read().decode('UTF-8'))
+
+if __name__ == '__main__':
+    main()
