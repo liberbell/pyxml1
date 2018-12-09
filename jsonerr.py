f4ac330c415cc87ea351e2abf8c8c68d87e62b9e
liberbell <liberbell@gmail.com>
2018-12-10 08:44:47 +0900

    jsonerr1

---
+++ jsonerr.py
@@ -0,0 +1,25 @@
+import json
+
+def main():
+    jsonStr = '''{
+        "sandwitch": "Reuben",
+        "toasted": true,
+        "toppings": [
+            "Thousand Island Dressing",
+            "SauerKraut",
+            "Pickles"
+        ],
+        "price": 8.99
+    }'''
+
+    data = json.loads(jsonStr)
+
+    print('sandwitch: ' + data['sandwitch'])
+    if (data['toasted']):
+        print('And it`s toasted!')
+
+    for topping in data['toppings']:
+        print('Topping: ' + topping)
+
+if __name__ == '__main__':
+    main()
