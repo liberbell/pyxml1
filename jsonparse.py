202883b5538a2858c772265ee40ccf54f923b84c
liberbell <liberbell@gmail.com>
2018-12-10 08:20:53 +0900

    jsonparse1

---
+++ jsonparse.py
@@ -0,0 +1,13 @@
+import json
+
+def main():
+    jsonStr = '''{
+        'sandwitch': 'Reuben,
+        'toasted': true,
+        'toppings': [
+            'Thousand Island Dressing',
+            'SauerKraut',
+            'Pickles'
+        ],
+        'price': 8.99
+    }'''
