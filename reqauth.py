e9076c3b8251e496a0e2f9b3f510b20d10916953
liberbell <liberbell@gmail.com>
2018-12-09 17:58:21 +0900

    reqauth2

--- reqauth.py
+++ reqauth.py
@@ -1,10 +1,15 @@
 import requests
 
 def main():
-    url ='http://httpbin.org/basic-auth/'
+    url ='http://httpbin.org/basic-auth/JoeMarini/MySecretWord'
 
     printResults(result)
 
 def printResults(resData):
     print('Resutl code: {0}'.format(resData.status_code))
     print('\n')
+
+    print('Returned data: ----------------')
+    print(resData.tesxt)
+
+    print('')
