ca29849b4715c4486fdc17248c7fd5a8124ce83c
liberbell <liberbell@gmail.com>
2018-12-09 17:37:07 +0900

    reqerr2

--- reqerr.py
+++ reqerr.py
@@ -3,3 +3,12 @@ import requests
 def main():
     url = 'http://httpbin.org/status/404'
     result = requests.get(url)
+    printResults(result)
+
+
+def printResults(resData):
+    print('Result code: {0}'.format(resData.status_code))
+    print('\n')
+
+    print('Returned Data:--------------------')
+    print(resData.text)
