0acf7b39ec698d941dc873347de8e8cd2f8f5870
liberbell <liberbell@gmail.com>
2018-12-10 08:34:49 +0900

    jsonseliarize2

---
+++ jsonseliarize.py
@@ -0,0 +1,14 @@
+import json
+
+def main():
+    pythonData = {
+        "sandwitch": "Reuben",
+        "toasted": True,
+        "toppings": ["Thousand Island Dressing",
+                     "SauerKraut",
+                     "Pickles"
+                     ],
+        "price" : 8.99
+    }
+
+    print('JSON Data:--------------------')
