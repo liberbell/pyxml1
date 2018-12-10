3e4ca8194d67606e393193f30f4d7aa85d7b1d9d
liberbell <liberbell@gmail.com>
2018-12-11 07:56:31 +0900

    saxparse3

--- saxparse.py
+++ saxparse.py
@@ -5,3 +5,15 @@ class MyContentHandler(xml.sax.ContentHandler):
     def __init__(self):
         self.slideCount = 0
         self.itemCount = 0
+
+def main():
+    handler = MyContentHandler()
+
+    url = 'http://httpbin.org/xml'
+    result = requests.get(url)
+
+    print('There are {0} slide elements.'.format(handler.slideCount))
+    print('There are {0} item elements.'.format(handler.itemCount))
+
+if __name__ == '__main__':
+    main()
