d650201968ff16c5b6b27f975165dbe39604fdd3
liberbell <liberbell@gmail.com>
2018-12-11 07:39:08 +0900

    jsonreq3

--- jsonreq.py
+++ jsonreq.py
@@ -5,5 +5,8 @@ def main():
     url = 'http://httpbin.org/json'
     result = requests.get(url)
 
+    dataobj = result.json()
+    print(json.dumps(dataobj, indent=4))
+
 if __name__ == '__main__':
     main()
