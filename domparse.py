08eebd92bd74d092a552e01d26c1cda7efa0baad
liberbell <liberbell@gmail.com>
2018-12-13 08:37:35 +0900

    domparse5

--- domparse.py
+++ domparse.py
@@ -9,6 +9,7 @@ def main():
     rootnode = domtree.documentElement
 
     print('The Root element is {0}'.format(rootnode.nodeName))
+    print('Title: {0}'.format(rootnode.getAttribute('title')))
 
 if __name__ == '__main__':
     main()
