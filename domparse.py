d873023c34c19961bb4414d6a3c411b0adaf8fae
liberbell <liberbell@gmail.com>
2018-12-13 08:35:09 +0900

    domparse3

--- domparse.py
+++ domparse.py
@@ -8,4 +8,7 @@ def main():
     domtree = xml.dom.minidom.parseString(result)
     rootnode = domtree.documentElement
 
-    print('Root element ')
+    print('The Root element is {0}'.format(rootnode.nodeName))
+
+if __name__ == '__main__':
+    main()
