1789c06199c997c2825ede2b29da70d6615a5ba9
liberbell <liberbell@gmail.com>
2018-12-13 09:11:35 +0900

    lxmlparse1

---
+++ lxmlparse.py
@@ -0,0 +1,12 @@
+import requests
+from lxml import etree
+
+def main():
+    url = 'http://httpbin.org/xml'
+    result = requests.get(url)
+
+    doc = etree.fromstring(result.content)
+    print(result.text)
+
+if __name__ == '__main__':
+    main()
