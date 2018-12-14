1ec1b9d7c3b14fd6a547ff52ed925d029f36d801
liberbell <liberbell@gmail.com>
2018-12-14 09:08:07 +0900

    lxmlparse9

--- lxmlparse.py
+++ lxmlparse.py
@@ -14,13 +14,17 @@ def main():
     for elem in doc.findall("slide"):
         print(elem.tag)
 
+    slideCount1 = len(doc.findall("slide"))
+
+
     newSlide = etree.SubElement(doc, "slide")
     newSlide.text = "This is a new slide"
 
-    slideCount = len(doc.findall("slide"))
+    slideCount2 = len(doc.findall("slide"))
     itemCount = len(doc.findall(".//item"))
 
-    print('There are {0} slide elements'.format(slideCount))
+    print('There are {0} slide elements'.format(slideCount1))
+    print('There are {0} slide elements'.format(slideCount2))
     print('There are {0} item elements'.format(itemCount))
 
 if __name__ == '__main__':
